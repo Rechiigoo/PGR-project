2569f30c00132fe2834067e0b4cfc050cbd18483
dormon <imilet@fit.vutbr.cz>
2023-11-02 07:46:36 +0100

    GLSL version is now obtained using glGetString

--- src/framework/defineGLSLVersion.cpp
+++ src/framework/defineGLSLVersion.cpp
@@ -1,6 +1,7 @@
 #include<framework/defineGLSLVersion.hpp>
 #include<sstream>
 #include<geGL/StaticCalls.h>
+#include<cstring>
 
 using namespace ge::gl;
 
@@ -8,8 +9,15 @@ std::string defineGLSLVersion(){
   std::stringstream ss;
   GLint major;
   GLint minor;
-  glGetIntegerv(GL_MAJOR_VERSION,&major);
-  glGetIntegerv(GL_MAJOR_VERSION,&minor);
+  auto ver = std::string((char const*)glGetString(GL_SHADING_LANGUAGE_VERSION));
+  if(ver.length()>2){
+    major = (GLint)(ver[0]-'0');
+    minor = (GLint)(ver[2]-'0');
+  }else{
+    glGetIntegerv(GL_MAJOR_VERSION,&major);
+    glGetIntegerv(GL_MAJOR_VERSION,&minor);
+  }
+
   ss << "#version ";
   ss << major;
   ss << minor;
